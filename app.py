750ec76fbb92b6f4b58c5fc6788bbc4d2c8ec8ea
msb <ben.sh@tohoku.ac.jp>
2025-11-22 21:07:14 +0800

    license and developer name added to source 2

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# Developed by Mehrdad S. Beni, Haipeng Liu, Gary Tse at HKMU, Hong Kong - 2025
+# Distributed under GPL V3 license, THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
+# Please see LICENSE file in the program repository
 
 from ba_gui import main
 
